e48efdc79491f991fb881505f90716c7145ef0ff
Cristian D Biancotti <cdbiancotti@gmail.com>
2022-06-29 19:00:33 -0300

    Playground intermedio Part 2

--- desafio/urls.py
+++ desafio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import una_vista, un_template
 
 urlpatterns = [
-    path('', una_vista),
-    path('mi-template/', un_template),
+    path('', una_vista, name='index'),
+    path('mi-template/', un_template, name='mi_template'),
 ]
--- desafio/views.py
+++ desafio/views.py
@@ -6,7 +6,7 @@ from .models import Prueba
 # Create your views here.
 
 def una_vista(request):
-    return HttpResponse('<h1>Mi perro dinamita</h1>')
+    return render(request, 'index.html')
 
 def un_template(request):
     
@@ -22,4 +22,4 @@ def un_template(request):
     # render = template.render({'lista_objetos': [prueba1, prueba2, prueba3]})
     # return HttpResponse(render)
     
-    return render(request, 'index.html', {'lista_objetos': [prueba1, prueba2, prueba3]})
+    return render(request, 'mi_template.html', {'lista_objetos': [prueba1, prueba2, prueba3]})
